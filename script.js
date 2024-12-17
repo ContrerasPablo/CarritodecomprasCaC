b10c943a4cc9329b0cad54f275eb0421978099f7
Pablo Contreras <Contreras.pabloarg@hotmail.com>
2024-12-17 19:02:57 -0300

    implementando primeros cambios con la tienda funcional
    Signed-off-by: Pablo Contreras <Contreras.pabloarg@hotmail.com>

--- script.js
+++ script.js
@@ -1,15 +1,73 @@
-/*const video = document.getElementById('miVideo');
-let lastScrollY = window.scrollY; // Almacenamos la posición inicial del scroll
+const productos =[
+  {
+    nombre:"Albahaca",
+    imagen:"imagenes/Albahaca.jpg",
+    precio:20
+  },
+  {
+    nombre:"Aji Picante",
+    imagen:"imagenes/Aji-Picante.jpg",
+    precio:20
+  },
+  {
+    nombre:"Aloe Vera",
+    imagen:"imagenes/Aloe-vera.jpg",
+    precio:20
+  },
+  {
+    nombre:"Limonero",
+    imagen:"imagenes/Limonero.jpg",
+    precio:70
+  },
+  {
+    nombre:"Menta",
+    imagen:"imagenes/Menta.jpg",
+    precio:20
+  },
+  {
+    nombre:"Nispero",
+    imagen:"imagenes/Nispero.jpg",
+    precio:70
+  },
+  {
+    nombre:"Palta",
+    imagen:"imagenes/Palta.jpg",
+    precio:70
+  },
+  {
+    nombre:"Romero",
+    imagen:"imagenes/Romero.jpg",
+    precio:70
+  },
+  {
+    nombre:"Ruda",
+    imagen:"imagenes/Ruda.jpg",
+    precio:20
+  },
+  {
+    nombre:"Sansevieria",
+    imagen:"imagenes/Sansevieria.jpg",
+    precio:20
+  },
+]
 
-function handleScroll() {
-  const currentScrollY = window.scrollY;
-  if (currentScrollY > lastScrollY) {
-    // Si el usuario está scrolling hacia abajo
-    video.pause();
-  } else {
-    // Si el usuario está scrolling hacia arriba
-    video.play();
-  }
-  lastScrollY = currentScrollY; // Actualizamos la última posición de scroll
-}
-window.addEventListener('scroll', handleScroll); */
+let plantasHTML ="";
+
+for (let i = 0; i < productos.length; i++) {
+  plantasHTML +=`
+            <div class="card producto__conteiner" >
+                <img src=${productos[i].imagen} class="card-img-top producto__img" alt="Plata de Albahaca">
+                <div class="card-body producto__info">
+                    <p class="producto__info--titulo">${productos[i].nombre}</p>
+                    <p class="card-text producto__info--precio">${productos[i].precio}</p>
+                    <a href="#" class="btn btn-light producto__info--boton ">Comprar</a>
+                </div>
+            </div>
+                
+`;
+};
+
+
+
+const contenedorPlantas = document.getElementById("contenedorPlantas");
+contenedorPlantas.innerHTML = plantasHTML;
